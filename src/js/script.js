387d55ac8bed57331cd35e62593f83b104809f50
Hisime <rihisime@gmail.com>
2018-03-05 19:48:34 +0300

    инициализация owl-carousel

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,13 @@ $(document).ready(function() {
   $('.page-header__toggler').click( function() {
     $(this).toggleClass('page-header__toggler--open');
     $('.page-header__right').slideToggle();
-  })
+  });
+
+  $('.hero-header').owlCarousel({
+    items:1,
+    smartSpeed: 500,
+    loop: true,
+    nav: false
+  });
 
 });
